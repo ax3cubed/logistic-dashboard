efdf94a7ce60e77f2d4b8737a0c23a8f963e00be
aakinwole <120341598+aakinwole@users.noreply.github.com>
2024-02-08 17:35:28 +0000

    installed vite, tailwind + react

--- src/App.jsx
+++ src/App.jsx
@@ -4,31 +4,30 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+ 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className=''>
+      <figure class="bg-amber-50 rounded-xl p-8 dark:bg-yellow-950">
+        <img class="w-24 h-24 rounded-full mx-auto hover:animate-pulse" src="https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg" alt="" width="384" height="512"/>
+          <div class="pt-6 space-y-4 text-white">
+            <blockquote>
+              <p class="text-lg font-medium">
+                “Tailwind CSS is the only framework that I've seen scale
+                on large teams. It’s easy to customize, adapts to any design,
+                and the build size is tiny.”
+              </p>
+            </blockquote>
+            <figcaption class="font-medium">
+              <div>
+                Sarah Dayan
+              </div>
+              <div>
+                Staff Engineer, Algolia
+              </div>
+            </figcaption>
+          </div>
+      </figure>
+    </div>
   )
 }
 
